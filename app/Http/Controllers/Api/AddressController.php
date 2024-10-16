d2665734c5ace8e8d10f4769f81d249a04c20887
petersudario <pedrohesudario@hotmail.com>
2024-10-16 00:53:52 -0300

    Addres Controller -> Return Geographic Coordinates

--- app/Http/Controllers/Api/AddressController.php
+++ app/Http/Controllers/Api/AddressController.php
@@ -4,20 +4,23 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Services\ViaCepService;
+use App\Services\GoogleMapsService;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 
 class AddressController extends Controller
 {
     protected ViaCepService $viaCepService;
+    protected GoogleMapsService $googleMapsService;
 
-    public function __construct(ViaCepService $viaCepService)
+    public function __construct(ViaCepService $viaCepService, GoogleMapsService $googleMapsService)
     {
         $this->viaCepService = $viaCepService;
+        $this->googleMapsService = $googleMapsService;
     }
 
     /**
-     * Armazena ou retorna o endereço baseado no CEP.
+     * Retorna o endereço baseado no CEP, incluindo coordenadas geográficas.
      *
      * @param Request $request
      * @return JsonResponse
@@ -34,6 +37,17 @@ class AddressController extends Controller
 
         if (!$address) {
             return response()->json(['message' => 'CEP não encontrado.'], 404);
+        }
+
+        $fullAddress = "{$address['logradouro']}, {$address['bairro']}, {$address['localidade']}, {$address['uf']}";
+        $coordinates = $this->googleMapsService->getCoordinates($fullAddress);
+
+        if ($coordinates) {
+            $address['lat'] = $coordinates['lat'];
+            $address['lng'] = $coordinates['lng'];
+        } else {
+            $address['lat'] = null;
+            $address['lng'] = null;
         }
 
         return response()->json($address, 200);
